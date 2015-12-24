685bb6639d51b2a232057189cb3692ebd4cb7f3d
sciencefans <sciencefans@foxmail.com>
2015-12-24 23:57:30 +0800

    =——=

--- NetVlad/netvlad_v100/yul_train.m
+++ NetVlad/netvlad_v100/yul_train.m
@@ -1,6 +1,6 @@
 setup;
 netID= 'vd16_tokyoTM_conv5_3_vlad_preL2_intra_white';
 paths= yul_localPaths();
-dbTrain= dbTokyoTimeMachine('train');
-dbVal= dbTokyoTimeMachine('val');
+dbTrain= get_ucf101('trainlist01.txt');
+dbVal= get_ucf101('testlist01.txt');
 lr= 0.0001;
