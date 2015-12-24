b377c1c5808775c048f3755743f57091e898a862
sciencefans <sciencefans@foxmail.com>
2015-12-25 00:08:01 +0800

    add get ucf101

---
+++ NetVlad/netvlad_v100/yul_get_ucf101.m
@@ -0,0 +1,9 @@
+function db = yul_get_ucf101(paths, listf)
+    annodir = fullfile(paths.dsetSpecDir, 'ucf101', listf);
+    list = textread(annodir, '%s');
+    if ~isempty(strfind(listf, 'train'))
+        db.label = list(2:2:end);
+        db.list = list(1:2:end);
+        assert(numel(db.label) ~= numel(db.list), 'DB load error: list and label size not match!');
+    end
+end
